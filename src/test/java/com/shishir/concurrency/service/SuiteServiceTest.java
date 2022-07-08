ebaba237bbf3178b75efae5945373f485bb4a3cd
Shishir Karki <shishir.karki@renegadeinsurance.com>
2022-07-08 10:10:17 +0545

    CHOR: optimize import

--- src/test/java/com/shishir/concurrency/service/SuiteServiceTest.java
+++ src/test/java/com/shishir/concurrency/service/SuiteServiceTest.java
@@ -5,10 +5,7 @@ import com.shishir.concurrency.repository.SuiteRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
