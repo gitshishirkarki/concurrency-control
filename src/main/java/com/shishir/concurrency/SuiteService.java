282648f4c2422ad1ddfc9debca49369a462922c2
Shishir Karki <shishir.karki@renegadeinsurance.com>
2022-07-08 08:41:16 +0545

    Feat: add suite service

---
+++ src/main/java/com/shishir/concurrency/SuiteService.java
@@ -0,0 +1,37 @@
+package com.shishir.concurrency;
+
+import com.shishir.concurrency.entities.Suite;
+import com.shishir.concurrency.repository.SuiteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SuiteService {
+    @Autowired
+    private SuiteRepository suiteRepository;
+
+    public Optional<Suite> findOne(Long id) {
+        return suiteRepository.findById(id);
+    }
+
+    public List<Suite> findAllAvailableSuites() {
+        return suiteRepository.findAllByStatus("AVAILABLE");
+    }
+
+    @Transactional
+    public Suite bookSuite(Long id, String user) {
+        Optional<Suite> suiteOptional = findOne(id);
+        if (suiteOptional.isPresent()) {
+            Suite suite = suiteOptional.get();
+            suite.setStatus("BOOKED");
+            suite.setBookedBy(user);
+
+            return suiteRepository.save(suite);
+        }
+        return null;
+    }
+}
--- src/main/java/com/shishir/concurrency/controller/SuiteController.java
+++ src/main/java/com/shishir/concurrency/controller/SuiteController.java
@@ -1,38 +1,31 @@
 package com.shishir.concurrency.controller;
 
+import com.shishir.concurrency.SuiteService;
 import com.shishir.concurrency.entities.Suite;
-import com.shishir.concurrency.repository.SuiteRepository;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/suites")
 public class SuiteController {
     @Autowired
-    private SuiteRepository suiteRepository;
+    private SuiteService suiteService;
 
     @GetMapping
-    public ResponseEntity<List<Suite>> getAvailableSuites(){
-        return new ResponseEntity<>(suiteRepository.findAllByStatus("AVAILABLE"), HttpStatus.OK);
+    public ResponseEntity<List<Suite>> getAvailableSuites() {
+        return new ResponseEntity<>(suiteService.findAllAvailableSuites(), HttpStatus.OK);
     }
 
     @PutMapping("/{id}/{user}")
-    public ResponseEntity<Suite> bookSuite(@PathVariable(name = "id", required = true) Long id, @PathVariable(name = "user", required = true) String user){
-        Optional<Suite> suiteOptional = suiteRepository.findById(id);
-        if(suiteOptional.isPresent()){
-            Suite suite = suiteOptional.get();
-            suite.setStatus("BOOKED");
-            suite.setBookedBy(user);
-
-            return new ResponseEntity<>(suiteRepository.save(suite), HttpStatus.OK);
+    public ResponseEntity<Suite> bookSuite(@PathVariable(name = "id", required = true) Long id, @PathVariable(name = "user", required = true) String user) {
+        Suite suite = suiteService.bookSuite(id, user);
+        if (null != suite) {
+            return new ResponseEntity<>(suite, HttpStatus.OK);
         }
-
         return new ResponseEntity<>(new Suite(), HttpStatus.NOT_ACCEPTABLE);
     }
 }
